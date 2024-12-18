5955c47145bc04a6703747d94a10ef2e3cb72a96
Ailton_78 <sd.ailtonsampaio@gmail.com>
2024-12-18 11:54:43 -0300

    finalization

---
+++ src/main/java/com/example/controleDeAcessoJwtOAuth2/ControleDeAcessoJwtOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.example.controleDeAcessoJwtOAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeAcessoJwtOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeAcessoJwtOAuth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/controleDeAcessoJwtOAuth2/ControleDeAcessoJwtOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controleDeAcessoJwtOAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeAcessoJwtOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
